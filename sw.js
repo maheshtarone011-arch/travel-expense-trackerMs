1959b7951bb51ac2562a4f186746c82a62877f7b
maheshtarone011-arch <maheshtarone011@gmail.com>
2026-02-15 00:23:42 +0530

    Add PWA support with icons, manifest and service worker

--- sw.js
+++
@@ -1,9 +0,0 @@
-
-// This file should be deleted.
-// Service Worker functionality has been removed.
-self.addEventListener('install', () => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', () => {
-  self.registration.unregister();
-});
